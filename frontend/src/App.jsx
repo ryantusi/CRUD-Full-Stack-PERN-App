7256607c51e98c917a8e8e106d034dafe84ec321
Ryan Tusi <ryantusi45@gmail.com>
2025-09-04 19:31:09 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import TableList from './components/TableList'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <TableList />
+    </>
+  )
+}
+
+export default App
